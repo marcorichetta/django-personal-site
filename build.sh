72f2a59995ededa3286d11045eed9127bfc3e3ae
Marco Richetta <marcorichetta@gmail.com>
2025-07-08 01:34:21 +0200

    uv has support on render

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-uv pip install -r requirements.txt
+uv sync
 
 # Convert static asset files
 python manage.py collectstatic --no-input
